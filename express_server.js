cb6ee74fc6eee52b522d74ce10c2544f933e5efe
Dango3010 <thuyoanh199930@gmail.com>
2022-12-08 00:46:33 +0000

    adding post request to have form submission, print out req.body in the terminal, and server sends OK back

--- express_server.js
+++ express_server.js
@@ -4,6 +4,18 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs"); //tells the Express app to use EJS as its templating engine.
 
+//for a post request, form submission:
+app.use(express.urlencoded({ extended: true }));
+//this body-parser library will convert the request body from a Buffer into string that we can read
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  //its output: in the template, we specified this longURL key using the input attribute name. The value is the content from the input field. This lovely formatting is courtesy of the Express library!
+  res.send("Ok"); // Respond with 'Ok'
+});
+
+function generateRandomString() {}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -12,6 +24,11 @@ const urlDatabase = {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase }; //urls = the key of the variable urlDatabase that we wanna put into the HTML file
   res.render("urls_index", templateVars); //name of the template + an object
+});
+
+//a GET route that renders the page with the form to present the form to the user
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => { 
