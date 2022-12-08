58c983bb0faf4cf283918fd36b1a98cf58fd9b7f
Dango3010 <thuyoanh199930@gmail.com>
2022-12-08 03:40:48 +0000

    Update expressserver.js to store ID-longurl key-value pair to database when a POST request is received to /urls and for the server to respond with a redirect from /urls to /urls/:id

--- express_server.js
+++ express_server.js
@@ -9,12 +9,30 @@ app.use(express.urlencoded({ extended: true }));
 //this body-parser library will convert the request body from a Buffer into string that we can read
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  //its output: in the template, we specified this longURL key using the input attribute name. The value is the content from the input field. This lovely formatting is courtesy of the Express library!
-  res.send("Ok"); // Respond with 'Ok'
+  console.log('req.body:', req.body); //req.body = the POST request body = the long URL we submit
+  //its output: in the urls_new template, we specified this longURL key using the input attribute name. The value is the content from the input field. This lovely formatting is courtesy of the Express library!
+  const id = generateRandomString();
+  urlDatabase[id] = req.body.longURL; //add new id-longURL key-value pair to the object
+  res.redirect(`/urls/${id}`); //when the server receives a POST request to /urls, it responds with a redirection to /urls/:id.
 });
 
-function generateRandomString() {}
+//randomly create a short URL id
+function generateRandomString() {
+    var result = '';
+    const length = 6;
+    const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+    return result;
+}
+
+app.get("/u/:id", (req, res) => {
+  const urlId = req.params.id; // = shortened URL = b2xVn2 / 9sm5xK
+  const longURL = urlDatabase[urlId];
+  res.redirect(longURL);
+});
+//e.g. when we click the link: http://localhost:8080/u/b2xVn2, we will be taken to http://www.lighthouselabs.ca.
+//the short URL ID links have /u/id by default by the urls_show template 
+//we can also test it by using curl command in the terminal 
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -22,6 +40,7 @@ const urlDatabase = {
 }; //used to keep track of all the URLs and their shortened forms. 
 
 app.get("/urls", (req, res) => {
+  console.log('urlDatabase:', urlDatabase);
   const templateVars = { urls: urlDatabase }; //urls = the key of the variable urlDatabase that we wanna put into the HTML file
   res.render("urls_index", templateVars); //name of the template + an object
 });
