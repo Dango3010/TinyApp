0db43825e1078e58a060908c799c9de0f3f47356
Dango3010 <thuyoanh199930@gmail.com>
2022-12-17 15:20:05 +0000

    stronger security on urls/:id/edit and urls/:id/delete pages

--- express_server.js
+++ express_server.js
@@ -17,20 +17,33 @@ app.post("/urls", (req, res) => {
   console.log('req.body:', req.body); //req.body = the POST request body = the long URL we submit
   //its output: in the urls_new template, we specified this longURL key using the input attribute name. The value is the content from the input field. This lovely formatting is courtesy of the Express library!
   const id = generateRandomString();
-  urlDatabase[id] = req.body.longURL; //add new id-longURL key-value pair to the object
+  urlDatabase[id] = {
+    'longURL': req.body.longURL,
+    'userID': userID
+  }; //add new id-longURL key object-key pair to the object
   res.redirect(`/urls/${id}`); //when the server receives a POST request to /urls, it responds with a redirection to /urls/:id.
 });
 
 //to handle the edit button
 app.post("/urls/:id/edit", (req, res) => {
   const urlId = req.params.id; // = shortened URL = b2xVn2 / 9sm5xK
-  urlDatabase[urlId] = req.body.longURL; //update the longURL of an ID
+  const userID = req.cookies['user_id'];
+  if (!urlDatabase[urlId]) return res.send('the URL id does not exist');
+  if(!userID) return res.send('you need to login or register first');
+  if (urlDatabase[urlId].userID !== userID) return res.send('you do not own this URL page');
+
+  urlDatabase[urlId].longURL = req.body.longURL; //update the longURL of an ID
   res.redirect("/urls"); //redirect to the home page
 });
 
 //to handle the delete button
 app.post("/urls/:id/delete", (req, res) => {
   const urlId = req.params.id; // = shortened URL = b2xVn2 / 9sm5xK
+  const userID = req.cookies['user_id'];
+  if (!urlDatabase[urlId]) return res.send('the URL id does not exist');
+  if(!userID) return res.send('you need to login or register first');
+  if (urlDatabase[urlId].userID !== userID) return res.send('you do not own this URL page');
+
   delete urlDatabase[urlId]; //remove the URLs and its id
   res.redirect("/urls"); //redirect to the home page
 });
@@ -57,7 +70,7 @@ app.post("/login", (req, res) => {
   if(!user) return res.status(403).send('user not found');
   if(user && password !== user.password) return res.status(403).send('user not found');
 
-  res.cookie('user_id', user.id); //create a current user id on the cookie
+  res.cookie('user_id', user.id); //create a current user id on cookie
   console.log('users:', users);
   res.redirect("/urls");
 });
@@ -120,9 +133,22 @@ function generateRandomString() {
     return result;
 }
 
+//filter out logged in user's URLs list
+const urlsForUser = (id) => {
+  let urlList = {};
+  for (url in urlDatabase) {
+    if (urlDatabase[url].userID === id) {
+      urlList[url] = urlDatabase[url];
+    }
+  }
+  return urlList; //an object of URLs of a particular user
+};
+
 app.get("/u/:id", (req, res) => {
   const urlId = req.params.id; // = shortened URL = b2xVn2 / 9sm5xK
-  const longURL = urlDatabase[urlId];
+  if (!urlDatabase[urlId]) return res.send('The url id cannot be found');
+
+  const longURL = urlDatabase[urlId].longURL;
   res.redirect(longURL);
 });
 //e.g. when we click the link: http://localhost:8080/u/b2xVn2, we will be taken to http://www.lighthouselabs.ca.
@@ -130,14 +156,23 @@ app.get("/u/:id", (req, res) => {
 //we can also test it by using curl command in the terminal 
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
 }; //used to keep track of all the URLs and their shortened forms. 
 
 app.get("/urls", (req, res) => {
   console.log('urlDatabase:', urlDatabase);
   const userID = req.cookies['user_id']
-  const templateVars = { urls: urlDatabase, user: users[userID] }; //urls = the key of the variable urlDatabase that we wanna put into the HTML file
+  if (!userID) return res.send('you need to login or register for an account to view the URLs list')
+
+  const urlList = urlsForUser(userID);
+  const templateVars = { urls: urlList, user: users[userID] };
   res.render("urls_index", templateVars); //name of the template + an object
 });
 
@@ -153,8 +188,11 @@ app.get("/urls/:id", (req, res) => {
   //the : in front of id means that id is a route parameter. 
   const urlId = req.params.id; // = shortened URL = b2xVn2 / 9sm5xK
   const userID = req.cookies['user_id'];
+  if (!userID) return res.send('you need to login to view the URL page')
+  if (urlDatabase[urlId].userID !== userID) return res.send('you do not own this URL page');
+
   const templateVars = { 
-    id: req.params.id, longURL: urlDatabase[urlId],
+    id: req.params.id, longURL: urlDatabase[urlId].longURL,
     user: users[userID]
   };
   res.render("urls_show", templateVars);
